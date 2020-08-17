3de9ce8e738a23f8bd8cee459c8cf154c8680a66
faymajun <823088775@qq.com>
2020-08-17 10:00:47 +0800

    testing

--- mongo/file_test.go
+++ mongo/file_test.go
@@ -8,21 +8,46 @@ import (
 
 type ServerInfo struct {
 	Name string `json:"name"`
-	Id string `json:"id"`
-	Time int64 `json:"time"`
+	Id   string `json:"id"`
+	Time int64  `json:"time"`
 }
 
 var (
-	TestDB = "db"
+	TestDB         = "db"
 	TestCollection = "collection"
 )
+
 func TestFile(t *testing.T) {
 	InitMongoFile("test")
-	testJson, err := json.Marshal(ServerInfo{Name: "testSever", Id:"1", Time: time.Now().Unix()})
+	testJson, err := json.Marshal(ServerInfo{Name: "testSever", Id: "1", Time: time.Now().Unix()})
 	if err != nil {
 		logger.Errorf("json marshal err: %v", err)
 	}
 	WriteLogOne(TestDB, TestCollection, string(testJson))
 	time.Sleep(2 * time.Second)
 	CloseMongoLog()
+}
+
+func TestFile1(t *testing.T) {
+	InitMongoFile("test")
+	for i := 0; i < 100; i++ {
+		WriteLogOne(TestDB, TestCollection, "aaaaaaaaaaa")
+	}
+	time.Sleep(3 * time.Second)
+	CloseMongoLog()
+}
+
+func TestFile2(t *testing.T) {
+	InitMongoFile("test")
+	for i := 0; i < 100; i++ {
+		WriteLogOne(TestDB, TestCollection, "bbbbbbbbbb")
+	}
+	time.Sleep(3 * time.Second)
+	CloseMongoLog()
+}
+
+func Test3(t *testing.T) {
+	go TestFile1(t)
+	go TestFile2(t)
+	time.Sleep(4 * time.Second)
 }
